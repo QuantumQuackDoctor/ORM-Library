89b2b9206c86c108cf4e0d272615fba8a99580c0
zhansu98 <zhansu6598@gmail.com>
2021-08-23 01:01:40 -0400

    removed phone number as a required field for users

--- src/main/java/com/database/ormlibrary/user/UserEntity.java
+++ src/main/java/com/database/ormlibrary/user/UserEntity.java
@@ -37,7 +37,6 @@ public class UserEntity {
         return this.email != null && this.email.length() != 0
                 && this.firstName != null && this.firstName.length() != 0
                 && this.lastName != null && this.lastName.length() != 0
-                && this.phone != null && this.phone.length() != 0
                 && this.orderList != null && this.birthDate != null;
     }
 
